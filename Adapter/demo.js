d6db2af877c004e907b7ba1e4b69db4b1a9af6a4
Diana Sarpe <diana.sarpe96@gmail.com>
2018-12-11 19:34:09 +0200

    delete js fails

--- Adapter/demo.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var LightningToMicroUSBAdapter_1 = require("./Clases/LightningToMicroUSBAdapter");
-var Iphone_1 = require("./Clases/Iphone");
-var iphone = new Iphone_1.iPhone();
-var chargeAdaptor = new LightningToMicroUSBAdapter_1.LightningToMicroUSBAdapter(iphone);
-chargeAdaptor.useMicroUSB();
